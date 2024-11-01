32f973ecfd38a46231876f2af2816bf3537dc97d
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-11-01 21:25:07 +0545

    feat(API): Successfully integrated API. fix(loading): Added loading page till when the expected prop is made available

--- src/app/_components/Banner.jsx
+++ src/app/_components/Banner.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Loading from './loading'
 
 const Banner = ( {product} ) => {
 
+  if(!product){
+    return <Loading />
+  }
+
   // styles for banner
   const imgStyle = {
-    backgroundImage: `url(${product.images})`,
+    backgroundImage: `url(${product.images[0].src})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: `15rem auto`,
     backgroundPosition: 'center',
@@ -13,7 +18,7 @@ const Banner = ( {product} ) => {
   return (
 
     <div className="flex relative text-center">
-        <h1 className="text-3xl tracking-wider text-gray-800 dark:text-gray-200 text-sha uppercase font-bold p-4 self-center z-10 content-center absolute text-center w-full md:text-4xl">Welcome to NovaNest </h1>
+        {/* <h1 className="text-3xl tracking-wider text-gray-800 dark:text-gray-200 text-sha uppercase font-bold p-4 self-center z-10 content-center absolute text-center w-full md:text-4xl">Welcome to NovaNest </h1> */}
         <div className=" h-72 object-cover block mx-auto  sm:block sm:w-full" style={imgStyle} >
         </div>
     </div>
