f22ea0cb7248e2388556aef14987e362c6260fd1
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-11-01 21:28:14 +0545

    fix(Currency): Replacted $ with NRS. fix(loading): Added a loading screen till when the prop is made availabel.

--- src/app/_components/Bill.jsx
+++ src/app/_components/Bill.jsx
@@ -1,33 +1,31 @@
 "use client"
 
-import { useProductAtCheckout } from "../_store/store"
+import { useProduct } from "../_store/ContextProvider";
 import { useState , useEffect } from "react";
+import Loading from "./loading";
 
 const Bill = ( { product } ) => {
     // Importing 'productTotal' function form useProductAtCheckout()
-    const { productTotal } = useProductAtCheckout();
-  // Creating a state variable for product's total price
-    const [ productPrice , setProductPrice ] = useState(0)
+    const { productTotal } = useProduct();
 
-    // upadting productPrice's value each time product and productTotal's values are changed. 
-    useEffect(()=>{
-        if (product) {
-            const calculated = productTotal(product.id);
-            setProductPrice(Math.round((calculated ?? 0) * 100) / 100);
-        } else {
-            setProductPrice(0); 
-        }
-    },[product , productTotal ])
+    console.log(product)
+
+    if( !product ){
+        return <Loading />
+    }
+
+    console.log("product: ",product)
+    console.log("Qty: ",product.userQuantity)
 
 
   return (
     <dl className="flex items-center justify-between gap-4 py-3">
         <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-            { product.title }
+            { product.name }
         </dt>
 
         <dd className="text-base font-medium text-gray-900 dark:text-white">
-            <span className="text-gray-500"> ${ product.price } X {product.userQuantity} : </span> ${ productPrice}
+            <span className="text-gray-500"> NRS. { product.price } X {product.userQuantity} : </span> NRS.{ Math.round( productTotal(product.id) * 100 )/100}
         </dd>
     </dl>
   )
