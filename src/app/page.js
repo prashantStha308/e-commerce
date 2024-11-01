3de8ee30a840d86cd530b5f7e03388de3820c0ac
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-11-01 21:34:51 +0545

    feat(API): Integrated API.fix: Made code a bit cleaner.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+// HOME PAGE
+
 "use client";
 import Header from "./_components/Header";
 import { useProduct } from "./_store/ContextProvider";
@@ -7,15 +9,17 @@ import Footer from "./_components/Footer";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Loading from "./_components/loading";
+import LowerBanner from "./_components/LowerBanner";
 
 
 export default function Home() {
 
   //Get all the products in contextProvider
   const { products = [] } = useProduct();
+  console.log("home",products)
 
   // using the first 4 products as banners for now
-  const bannerProducts = products
+  const bannerProducts = products.slice(0,2);
   const [ currentBanner , setCurrentBanner ] = useState(0);
 
   // For banner
@@ -36,7 +40,7 @@ export default function Home() {
 
       {/* Banner */}
       <div className="bg-white dark:bg-black">
-        {/* <Banner product={bannerProducts[currentBanner]} /> */}
+        <Banner product={bannerProducts[currentBanner]} />
       </div>
         
         {/* Product Tiles */}
@@ -62,22 +66,8 @@ export default function Home() {
 
       </div>
 
-      {/* Static unrelated banner */}
-      <div className="bg-gray-500 text-white dark:bg-gray-800">
-        <section className="py-5 mx-auto max-w-screen-xl  sm:flex flex-grow-0 sm:p-5">
-          <article className="flex-col justify-center inline-flex self-stretch  p-6">
-            <h2 className="uppercase font-semibold text-xl">Design</h2>
-            <strong className="text-lg pb-2 font-normal font-sans sm:text-2xl">Our 30 Year Journey</strong>
-            <p className="text-sm pb-4">Join us as we revisit our roots, tracing the path from bright idea to industry disruptors with a plan to fight climate change while creating beautiful things. </p>
-            <button className="font-semibold uppercase rounded-md border-white border p-3 max-w-max hover:bg-gray-600 hover:text-white">Join us</button>
-          </article>
-          <figure>
-            <Image 
-            src="https://images.unsplash.com/photo-1606170033648-5d55a3edf314?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGxpZ2h0c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" 
-            alt="Banner" looding="lazy" width={1080} height={520} />
-          </figure>
-        </section>
-      </div>
+      {products && <LowerBanner product={products[0]} />}
+
       <Footer />
     </div>
   );
